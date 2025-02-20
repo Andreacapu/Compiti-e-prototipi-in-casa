d7ec9b31b2788d835e7f93ae48b3d8e2c42e4adc
Andreacapu <capuani.andrea@gmail.com>
2025-02-21 00:04:37 +0100

    Add files via upload

---
+++ Esercizi drawIO and python/esercizio_3_8.py
@@ -0,0 +1,17 @@
+lista_località = ["vienna", "tokyo", "andorra", "pechino", "zurigo"]
+print("lista originale", lista_località)
+
+print("lista in ordine alfabetico",sorted(lista_località))
+
+print("lista in ordine originale dopo la modifica",sorted(lista_località, reverse = True))
+
+print("lista originale dopo la seconda inversione():", lista_località)
+
+print("lista in ordine invertito", lista_località[::-1])
+
+
+lista_località = ["vienna", "tokyo", "andorra", "pechino", "zurigo"]
+print = lista_località.sort()
+
+lista_località = ["vienna", "tokyo", "andorra", "pechino", "zurigo"]
+print =lista_località.sort(reverse = True)
---
+++ Esercizi drawIO and python/esercizio_3_9 (1).py
@@ -0,0 +1,5 @@
+lista_invitati = ["marco", "antonio", "sempronio", "samuele"]
+
+x = len(lista_invitati)
+
+print (x)
